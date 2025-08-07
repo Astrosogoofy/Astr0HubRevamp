00c3ac3499c0c7b3b89b81e7e63c9f5da6b478df
Astrosogoofy <archie.tibbenham@gmail.com>
2025-08-07 17:12:16 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -100,6 +100,8 @@ local Supported = {
     [286090429] = "Arsenal",
     [2753915549] = "Bloxfruits",
     [13772394625] = "Blade Ball",
+    [11605035507] = "The Strongest Battlegrounds",
+
 }
 
 -- Game Tab Generator
@@ -115,6 +117,19 @@ end
 
 -- Game-Specific Logic
 local id = game.PlaceId
+
+-- Then below, in the logic area:
+elseif Supported[id] == "The Strongest Battlegrounds" then
+    createGameTab("Strongest BG", nil, {
+        {
+            name = "Load DevHub",
+            callback = function()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/Scripts-Roblox/refs/heads/main/XDevHubBeta.lua"))()
+                notify("Strongest BG", "Script loaded.")
+            end
+        }
+    })
+
 
 if Supported[id] == "Forsaken" then
     createGameTab("Forsaken", nil, {
@@ -214,6 +229,7 @@ else
         }
     })
 end
+
 
 
 
